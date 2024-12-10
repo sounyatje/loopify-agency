f9d46999671f96cd265a01923b58b0daecd471c5
Sounyâ Abattouy <s.abattouy@interface3.be>
2024-12-10 16:03:40 +0100

    modif

--- front-end/src/components/home/Header.jsx
+++ front-end/src/components/home/Header.jsx
@@ -17,7 +17,7 @@ export default function Hero() {
     className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
   />
 </div>
-
+< div className="flex gap-6">
         <div className="mb-14 max-w-2xl py-32 sm:py-48 lg:py-56 bg-red-700">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -49,7 +49,12 @@ export default function Hero() {
             </div>
           </div>
         </div> 
+        <div className="flex-1 mb-14 py-32 sm:py-48 lg:py-56  bg-red-700">
+            {/* Ce div est vide et ajustable */}
+          </div>
         
+
+        </div>
         <div
   aria-hidden="true"
   className="absolute inset-x-0 -top-40 z-0 transform-gpu overflow-hidden blur-3xl sm:-top-80"
